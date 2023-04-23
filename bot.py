9f6c934c0c0b4959141c098fd63e6e171311b641
SirNatan1 <106090674+SirNatan1@users.noreply.github.com>
2023-04-23 13:34:44 +0300

    Added: python file for slack bot
    Added: the first file to the slack bot

---
+++ bot.py
@@ -0,0 +1,45 @@
+import os
+import time
+import schedule
+import logging
+import random
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+logging.basicConfig(level=logging.DEBUG)
+
+def sendMessage(slack_client, msg):
+  # make the POST request through the python slack client
+  
+  # check if the request was a success
+  try:
+    slack_client.chat_postMessage(
+      channel='#nihongo-practice',
+      text=msg
+    )#.get()
+  except SlackApiError as e:
+    logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
+    logging.error(e.response)
+
+if __name__ == "__main__":
+  SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
+  slack_client = WebClient(SLACK_BOT_TOKEN)
+  logging.debug("authorized slack client")
+
+  letters = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 
+           'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 
+           'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', 'を', 'ん', 'が', 'ぎ',
+           'ぐ', 'げ', 'ご', 'ざ', 'じ', 'ず', 'ぜ', 'ぞ', 'だ', 'ぢ', 'づ', 'で', 'ど', 'ば', 'び', 'ぶ',
+           'べ', 'ぼ', 'ぱ', 'ぴ', 'ぷ', 'ぺ', 'ぽ']
+  
+  # Choose three random Japanese letter
+  chosen_letters = random.sample(letters, 3)
+
+  msg = f"Konnichiwa mina-san kyo wa moji desu, gannbate kudasai: {chosen_letters[0]}, {chosen_letters[1]}, {chosen_letters[2]}"
+  schedule.every().day.at("11:00").do(lambda: sendMessage(slack_client, msg))
+
+  logging.info("entering loop")
+
+  while True:
+    schedule.run_pending()
+    time.sleep(5) # sleep for 5 seconds between checks on the scheduler
